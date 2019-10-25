0ddffe5cfb2b24428c5d386ca81bc092e431d935
Anwesh43 <anweshthecool0@gmail.com>
2019-10-25 09:24:06 +0530

    adding constants and libraries

--- linearcsweepview/src/main/java/com/anwesh/uiprojects/linearcsweepview/LineArcSweepView.kt
+++ linearcsweepview/src/main/java/com/anwesh/uiprojects/linearcsweepview/LineArcSweepView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.linearcsweepview
 /**
  * Created by anweshmishra on 25/10/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+
+val nodes : Int = 5
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val delay : Long = 20
+val foreColor : Int = Color.parseColor("#01579B")
+val backColor : Int = Color.parseColor("#BDBDBD")
