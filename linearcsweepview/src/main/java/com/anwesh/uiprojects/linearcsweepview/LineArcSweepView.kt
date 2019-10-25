859883cce307dd521b08078befbcd16cd8e4b5da
Anwesh43 <anweshthecool0@gmail.com>
2019-10-25 09:49:07 +0530

    adding LASNode class

--- linearcsweepview/src/main/java/com/anwesh/uiprojects/linearcsweepview/LineArcSweepView.kt
+++ linearcsweepview/src/main/java/com/anwesh/uiprojects/linearcsweepview/LineArcSweepView.kt
@@ -120,4 +120,46 @@ class LineArcSweepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LASNode(var i : Int, val state : State = State()) {
+
+        private var next : LASNode? = null
+        private var prev : LASNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LASNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLASNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LASNode {
+            var curr : LASNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
